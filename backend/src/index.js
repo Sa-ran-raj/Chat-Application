804da41be4953228212a6781a26b87f36f0112bc
Saran Raj <saranrajalakshmi2296@gmail.com>
2025-07-01 22:57:14 +0530

    Modified Index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,13 +28,14 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
+    // Serve static files FROM THE DIST FOLDER (not index.html)
     app.use(express.static(path.join(_dirname, "../frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,"../frontend","dist","index.html"));
-    })
-}
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+    });
+}
 server.listen(5001,()=>{
     console.log("server running on port 5001");
     connectDB();
