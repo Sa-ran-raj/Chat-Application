dbc77b3b6dbaa768058301bb643183078c49cea4
Saran Raj <saranrajalakshmi2296@gmail.com>
2025-07-01 22:54:28 +0530

    Modified Index.js for Production

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,8 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname, "../frontend","dist","index.html")));
-
+    app.use(express.static(path.join(_dirname, "../frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.join(_dirname,"../frontend","dist","index.html"));
     })
